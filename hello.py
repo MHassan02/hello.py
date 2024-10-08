c7064a42b5dbe93c620e924898bab497b6bd7c70
MHassan02 <mhassan007.mahmood@gmail.com>
2024-10-08 17:57:31 +0500

    Python hello code

---
+++ hello.py
@@ -0,0 +1,52 @@
+# print("HEllo")
+# name = "Hassan"
+# age = 23
+# print("My name is ",name,"my age is ", age)
+
+# A,B =2,3
+# txt ="@"
+# print(A*txt*B)
+
+# name = input("Enter name: ")
+# add = input("Enter address: ")
+# age = int(input("Enter age: "))
+# print(name,add,age,type(add),type(age))
+
+# light = input("Enter color: ")
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("look")
+# elif(light == "green"):
+#     print("go")
+# else:
+#     print("light is broken")
+
+# age = int(input("Enter age: "))
+# vote = ("yes","no") [age <= 18]
+# print(vote)
+
+# sal = float(input("Enter salary: "))
+# tax = sal*(0.2,0.1) [sal <= 50000]
+# print(tax)
+
+# str = "hello world"
+# str1 = "pakistan"
+# print(str + str1)
+
+# print(str1[2:6])
+# print(str1[-6:-2])
+# print(str1.capitalize())
+# print(str1)
+
+# print(str.replace("l","y"))
+# print(str)
+# name = input("enter name: ")
+# print(len(name))
+# print(name.count("a"))
+
+num = int(input("Enter number: "))
+if(num % 7 == 0):
+    print(num,"is divisible by 7")
+else:
+    print(num,"is not divisible by 7")
